8b160298686e609f841e525f39b984e328de06a2
Berkus <berkus@gmail.com>
2013-09-22 16:04:18 +0300

    Fix compilation flags.
    More permanent issue with boost.unit_test internal declarations.
    Put it into shared cmake init since it affects several projects.

--- setup.cmake
+++ setup.cmake
@@ -7,6 +7,9 @@ set(__cmake_setup_INCLUDED 1)
 # Enable C++14
 set(CMAKE_CXX_FLAGS "-ferror-limit=3 ${CMAKE_CXX_FLAGS} -std=c++1y -stdlib=libc++")
 
+# Nasty boost.unit_test_framework
+set(_extraFlags "${_extraFlags} -Wno-unneeded-internal-declaration")
+
 # On mac, use openssl from brew, not the default system one, because it is too old.
 # Run 'brew install openssl' to install it.
 if (APPLE)
