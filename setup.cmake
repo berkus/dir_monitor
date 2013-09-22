e85ded4c56ef9b8faecd63c56c6485124f40559c
Berkus <berkus@gmail.com>
2013-09-22 16:38:26 +0300

    Lots of boost stuff breaks with unused parameters warning.

--- setup.cmake
+++ setup.cmake
@@ -8,7 +8,7 @@ set(__cmake_setup_INCLUDED 1)
 set(CMAKE_CXX_FLAGS "-ferror-limit=3 ${CMAKE_CXX_FLAGS} -std=c++1y -stdlib=libc++")
 
 # Enable full error and warning reporting
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror -Wno-unused-parameter")
 
 # Nasty boost.unit_test_framework
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unneeded-internal-declaration")
