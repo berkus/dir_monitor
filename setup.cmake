60ab3d72fe19768904d7df0da401e213e7b45a0a
Berkus <berkus@gmail.com>
2013-11-01 01:13:57 +0200

    Disable clang invocations debug.

--- setup.cmake
+++ setup.cmake
@@ -18,7 +18,7 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror -Wno-unused-parame
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unneeded-internal-declaration")
 
 # Clang invocation debug
-set(CMAKE_CXX_FLAGS "-v ${CMAKE_CXX_FLAGS}")
+#set(CMAKE_CXX_FLAGS "-v ${CMAKE_CXX_FLAGS}")
 
 # On mac, use openssl from brew, not the default system one, because it is too old.
 # Run 'brew install openssl' to install it.
