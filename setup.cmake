6fa3b8c66da5bdb86c9344806faf76f352d53459
Berkus <berkus@gmail.com>
2013-12-19 03:00:39 +0200

    Add global constructors warning (disabled).

--- setup.cmake
+++ setup.cmake
@@ -17,6 +17,10 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror -Wno-unused-parame
 # Nasty boost.unit_test_framework
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unneeded-internal-declaration")
 
+# Test effect of global constructors in our code, global constructors are usually bad idea.
+# Cannot be enabled because boost uses them in boost.error_code
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wglobal-constructors")
+
 # Clang invocation debug
 #set(CMAKE_CXX_FLAGS "-v ${CMAKE_CXX_FLAGS}")
 
