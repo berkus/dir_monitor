8177988099e05dc8ac1f51421401292213a2104f
Berkus <berkus@gmail.com>
2014-01-07 00:50:08 +0200

    Will apparently need an fseventsd based implementation for OSX.

---
+++ include/fsevents/dir_monitor_impl.hpp
@@ -0,0 +1,3 @@
+FSEvents-based implementation for OSX
+
+https://developer.apple.com/library/mac/documentation/Darwin/Reference/FSEvents_Ref/Reference/reference.html#//apple_ref/doc/c_ref/kFSEventStreamCreateFlagFileEvents
