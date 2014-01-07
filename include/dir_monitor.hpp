034595dfffde96acc86296edc22abf578aaad247
Berkus <berkus@gmail.com>
2014-01-07 16:17:35 +0200

    Strip off trailing spaces.

--- include/dir_monitor.hpp
+++ include/dir_monitor.hpp
@@ -1,7 +1,7 @@
-// 
-// Copyright (c) 2008, 2009 Boris Schaeling <boris@highscore.de> 
-// 
-// Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//
+// Copyright (c) 2008, 2009 Boris Schaeling <boris@highscore.de>
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #pragma once
@@ -17,11 +17,11 @@
 #  error "Platform not supported." 
 #endif 
 
-namespace boost { 
-namespace asio { 
+namespace boost {
+namespace asio {
 
-typedef basic_dir_monitor<basic_dir_monitor_service<> > dir_monitor; 
+typedef basic_dir_monitor<basic_dir_monitor_service<> > dir_monitor;
 
-} 
-} 
+}
+}
 
