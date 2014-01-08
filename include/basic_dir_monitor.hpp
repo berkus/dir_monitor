904826c15cde18b01d89aeb96416d333c5764ce4
Berkus <berkus@gmail.com>
2014-01-08 13:48:18 +0200

    Strip off trailing spaces.

--- include/basic_dir_monitor.hpp
+++ include/basic_dir_monitor.hpp
@@ -1,16 +1,16 @@
-// 
-// Copyright (c) 2008, 2009 Boris Schaeling <boris@highscore.de> 
-// 
-// Distributed under the Boost Software License, Version 1.0. (See accompanying 
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) 
-// 
+//
+// Copyright (c) 2008, 2009 Boris Schaeling <boris@highscore.de>
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #pragma once
 
 #include <boost/asio.hpp> 
 #include <string> 
 
-namespace boost { 
-namespace asio { 
+namespace boost {
+namespace asio {
 
 struct dir_monitor_event 
 { 
@@ -24,8 +24,8 @@ struct dir_monitor_event
         renamed_new_name = 5 
     }; 
 
-    dir_monitor_event() 
-        : type(null) { } 
+    dir_monitor_event()
+        : type(null) { }
 
     dir_monitor_event(const std::string &d, const std::string &f, event_type t) 
         : dirname(d), filename(f), type(t) { } 
@@ -35,46 +35,46 @@ struct dir_monitor_event
     event_type type; 
 }; 
 
-template <typename Service> 
-class basic_dir_monitor 
-    : public boost::asio::basic_io_object<Service> 
-{ 
-public: 
-    explicit basic_dir_monitor(boost::asio::io_service &io_service) 
-        : boost::asio::basic_io_object<Service>(io_service) 
-    { 
-    } 
+template <typename Service>
+class basic_dir_monitor
+    : public boost::asio::basic_io_object<Service>
+{
+public:
+    explicit basic_dir_monitor(boost::asio::io_service &io_service)
+        : boost::asio::basic_io_object<Service>(io_service)
+    {
+    }
 
-    void add_directory(const std::string &dirname) 
-    { 
-        this->service.add_directory(this->implementation, dirname); 
-    } 
+    void add_directory(const std::string &dirname)
+    {
+        this->service.add_directory(this->implementation, dirname);
+    }
 
-    void remove_directory(const std::string &dirname) 
-    { 
-        this->service.remove_directory(this->implementation, dirname); 
-    } 
+    void remove_directory(const std::string &dirname)
+    {
+        this->service.remove_directory(this->implementation, dirname);
+    }
 
-    dir_monitor_event monitor() 
-    { 
-        boost::system::error_code ec; 
-        dir_monitor_event ev = this->service.monitor(this->implementation, ec); 
-        boost::asio::detail::throw_error(ec); 
-        return ev; 
-    } 
+    dir_monitor_event monitor()
+    {
+        boost::system::error_code ec;
+        dir_monitor_event ev = this->service.monitor(this->implementation, ec);
+        boost::asio::detail::throw_error(ec);
+        return ev;
+    }
 
-    dir_monitor_event monitor(boost::system::error_code &ec) 
-    { 
-        return this->service.monitor(this->implementation, ec); 
-    } 
+    dir_monitor_event monitor(boost::system::error_code &ec)
+    {
+        return this->service.monitor(this->implementation, ec);
+    }
 
-    template <typename Handler> 
-    void async_monitor(Handler handler) 
-    { 
-        this->service.async_monitor(this->implementation, handler); 
-    } 
-}; 
+    template <typename Handler>
+    void async_monitor(Handler handler)
+    {
+        this->service.async_monitor(this->implementation, handler);
+    }
+};
 
-} 
-} 
+}
+}
 
