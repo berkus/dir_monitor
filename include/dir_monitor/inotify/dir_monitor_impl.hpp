a6ee49bdd5bdd130fd95812be0ebf7391e412f18
André Kapelrud <a.kapelrud@gmail.com>
2016-04-01 14:11:02 +0200

    Translate IN_ATTRIB to IN_MODIFY
    Inotify has a separate event for attribute changes (modification times
    etc.). Translate this to a modification event, to be consistent with the
    windows implementation.

--- include/dir_monitor/inotify/dir_monitor_impl.hpp
+++ include/dir_monitor/inotify/dir_monitor_impl.hpp
@@ -41,7 +41,7 @@ public:
 
     void add_directory(const std::string &dirname)
     {
-        int wd = inotify_add_watch(fd_, dirname.c_str(), IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_FROM | IN_MOVED_TO);
+        int wd = inotify_add_watch(fd_, dirname.c_str(), IN_CREATE | IN_DELETE | IN_ATTRIB | IN_MODIFY | IN_MOVED_FROM | IN_MOVED_TO);
         if (wd == -1)
         {
             boost::system::system_error e(boost::system::error_code(errno, boost::system::get_system_category()), "boost::asio::dir_monitor_impl::add_directory: inotify_add_watch failed");
@@ -159,7 +159,9 @@ private:
                 {
                 case IN_CREATE: type = dir_monitor_event::added; break;
                 case IN_DELETE: type = dir_monitor_event::removed; break;
-                case IN_MODIFY: type = dir_monitor_event::modified; break;
+                case IN_ATTRIB:
+                case IN_MODIFY: type = dir_monitor_event::modified;
+                    break;
                 case IN_MOVED_FROM: type = dir_monitor_event::renamed_old_name; break;
                 case IN_MOVED_TO: type = dir_monitor_event::renamed_new_name; break;
                 case IN_CREATE | IN_ISDIR:
